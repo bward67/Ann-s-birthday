0d3acf04f3731fe4b9f0bd7ffe790f42605808c6
Barbara <subwaynaas@yahoo.ie>
2025-06-22 16:26:13 +0200

    h2

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,21 @@ const App = () => {
 
   return (
     <div>
-      <h1>Welcome, Ann Hounjet, to your very own game of Pair Snair!</h1>
-      <p className="grey-p">One of your Aunty Patsy's favorite games. </p>
-      <p>
-        You might recognize some of these SUPER STARS who look like they may
-        have special SUPER POWERS!
-      </p>
+      {hasWon ? (
+        <>
+          <h2>WINNER WINNER - CHICKEN DINNER!!</h2> <p>Why not Play Again?</p>
+        </>
+      ) : (
+        <>
+          <h1>Welcome, Ann Hounjet, to your very own game of Pair Snair!</h1>
+          <p className="grey-p">One of your Aunty Patsy's favorite games. </p>
+          <p>
+            You might recognize some of these SUPER STARS who look like they may
+            have special SUPER POWERS!
+          </p>
+        </>
+      )}
+
       <button className="start-game-btn" onClick={startGame}>
         {hasWon ? "Play Again" : "Start Game"}
       </button>
