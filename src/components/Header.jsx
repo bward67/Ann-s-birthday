2302ef9bfb29769b24e0009568b687c89383d7d4
Barbara <subwaynaas@yahoo.ie>
2025-07-02 16:29:34 +0200

    Changed title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <h1>Welcome, Ann Hounjet, to your very own game of Pair Snair!</h1>
+      <div className="title">
+        <h1>Welcome Hounjet Family, to your very own game of Pair Snair!</h1>
+      </div>
       <p className="grey-p">One of your Aunty Patsy's favorite games. </p>
       <p>
         You might recognize some of these SUPER STARS who look like they may
